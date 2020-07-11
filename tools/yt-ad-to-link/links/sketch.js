41f9d80910ac244e61f8077eddcd42792c131169
BenjaminAster <nimajneb0905@gmail.com>
2020-07-11 18:24:02 +0200

    -

--- tools/yt-ad-to-link/links/sketch.js
+++ tools/yt-ad-to-link/links/sketch.js
@@ -1,11 +1,32 @@
 
 
-function preload() {
-	let links = loadStrings("unlisted-ad-links.txt");
+let ids;
+let titles;
+let HTMLString = "";
+
+function getTitle(url, successCallback = function () { }) {
+	let title = "blablabla";
+	$.ajax({
+		url: `http://textance.herokuapp.com/title/${url}`,
+		complete: function (data) {
+			title = data.responseText;
+			successCallback(title);
+		}
+	});
 }
 
 function setup() {
-	print(links);
-	links = links.split("\n");
-	console.log(links);
+	ids = loadStrings("https://benjaminaster.github.io/tools/yt-ad-to-link/links/unlisted-ad-links.txt", success = function () {
+		ids.forEach(function (item, index) {
+			ids[index] = item.substring(32, item.length);
+		});
+		titles = Array(ids.length).fill("error");
+		for (let i = 0; i < ids.length; i++) {
+			getTitle(`https://youtube.com/embed/${ids[i]}`, successCallback = function (title) {
+				titles[i] = title.substring(0, title.length - 10);
+				document.querySelector(".urls").innerHTML += 
+					`<li> <a href="https://www.youtube.com/watch?v=${ids[i]}"> https://www.youtube.com/watch?v=${ids[i]} </a> &#9472 ${titles[i]} </li>\n`;
+			});
+		}
+	});
 }
