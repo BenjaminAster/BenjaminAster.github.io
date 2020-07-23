c7f1d8e0827b715df9853a946fd101f347add7e6
BenjaminAster <nimajneb0905@gmail.com>
2020-07-23 21:36:36 +0200

    -

--- go-to-url/sketch.js
+++ go-to-url/sketch.js
@@ -6,11 +6,20 @@ function setup() {
 
 function draw() {
 
-	
+
 }
 
 function keyPressed() {
-	if (keyCode == ENTER) {
+	if (key == 'b') {
+		open(document.getElementById("url-input").value, "_blank");
+	}
+	if (key == 's') {
 		open(document.getElementById("url-input").value, "_self");
+	}
+	if (key == 'p') {
+		open(document.getElementById("url-input").value, "_parent");
+	}
+	if (key == 't') {
+		open(document.getElementById("url-input").value, "_top");
 	}
 }
