c3a929d5b8562a5c75b02164eea926d1001d73e5
BenjaminAster <nimajneb0905@gmail.com>
2020-08-14 14:12:09 +0200

    -

--- 2048/game.js
+++ 2048/game.js
@@ -7,6 +7,7 @@ class Game {
 		this.animationTime = 300;
 		this.tileSet = false;
 		this.deletedTiles = [];
+		this.animationFinished = true;
 	}
 
 	windowResized() {
@@ -38,7 +39,7 @@ class Game {
 			}
 		}
 		if (emptySpots == 0) {
-			console.log("game over");
+			//console.log("game over");
 			this.grid = Array(this.gridUHei).fill().map(x => Array(this.gridUWid).fill(null));
 			this.newTile();
 		} else {
@@ -69,13 +70,13 @@ class Game {
 			}
 		}
 
-		let animationFinished = true;
+		this.animationFinished = true;
 
 		for (let row in this.grid) {
 			for (let clm in this.grid[0]) {
 				if (this.grid[row][clm] != null) {
 					this.grid[row][clm].show(false);
-					animationFinished = this.grid[row][clm].isAnimationFinished() && animationFinished;
+					this.animationFinished = this.grid[row][clm].isAnimationFinished() && this.animationFinished;
 					if (this.grid[row][clm].isAnimationFinished() && this.grid[row][clm].changeNumber) {
 						this.grid[row][clm].powerOf2++;
 						this.grid[row][clm].changeNumber = false;
@@ -85,7 +86,7 @@ class Game {
 		}
 
 		for (let i = this.deletedTiles.length - 1; i >= 0; i--) {
-			animationFinished = this.deletedTiles[i].isAnimationFinished() && animationFinished;
+			this.animationFinished = this.deletedTiles[i].isAnimationFinished() && this.animationFinished;
 			if (this.deletedTiles[i].isAnimationFinished()) {
 				this.deletedTiles.splice(i, 1);
 			} else {
@@ -94,7 +95,7 @@ class Game {
 		}
 
 
-		if (animationFinished && !this.tileAdded) {
+		if (this.animationFinished && !this.tileAdded) {
 			this.newTile();
 			this.tileAdded = true;
 		}
@@ -112,93 +113,95 @@ class Game {
 	}
 
 	swipe(dir) {
-		let gridTiles;
-		function updateGridTile(clm, row) {
-			game.grid[row][clm].move(clm, row);
-		}
-		if (dir == "up" || dir == "down") {
-			gridTiles = Array(this.gridUWid);
-
-			for (let clm in this.grid[0]) {
-				gridTiles[clm] = [];
-				for (let row in this.grid) {
-					if (this.grid[row][clm] != null) {
-						gridTiles[clm].push(this.grid[row][clm]);
-					}
-				}
+		if (this.animationFinished) {
+			let gridTiles;
+			function updateGridTile(clm, row) {
+				game.grid[row][clm].move(clm, row);
 			}
-			this.grid = Array(this.gridUHei).fill().map(x => Array(this.gridUWid).fill(null));
-			for (let clm = 0; clm < gridTiles.length; clm++) {
-				let prevPowerOf2 = 0;
-				for (let row = gridTiles[clm].length - 1; row >= 0; row--) {
-					if (gridTiles[clm][row].powerOf2 == prevPowerOf2) {
-						this.deletedTiles.unshift(new Tile(gridTiles[clm][row].uX, gridTiles[clm][row].uY, gridTiles[clm][row].powerOf2));
-						this.deletedTiles[0].siblingTile = gridTiles[clm][row + 1];
-						gridTiles[clm].splice(row, 1);
-						gridTiles[clm][row].changeNumber = true;
-						prevPowerOf2 = 0;
-					} else {
-						prevPowerOf2 = gridTiles[clm][row].powerOf2;
-					}
-				}
+			if (dir == "up" || dir == "down") {
+				gridTiles = Array(this.gridUWid);
 
-				for (let row = 0; row < gridTiles[clm].length; row++) {
-					if (dir == "up") {
-						this.grid[row][clm] = gridTiles[clm][row];
-						updateGridTile(clm, row);
-					} else {
-						this.grid[this.gridUHei - gridTiles[clm].length + row][clm] = gridTiles[clm][row];
-						updateGridTile(clm, this.gridUHei - gridTiles[clm].length + row);
-					}
-				}
-
-			}
-		}
-
-		if (dir == "left" || dir == "right") {
-			gridTiles = Array(this.gridUHei);
-
-			for (let row in this.grid) {
-				gridTiles[row] = [];
 				for (let clm in this.grid[0]) {
-					if (this.grid[row][clm] != null) {
-						gridTiles[row].push(this.grid[row][clm]);
+					gridTiles[clm] = [];
+					for (let row in this.grid) {
+						if (this.grid[row][clm] != null) {
+							gridTiles[clm].push(this.grid[row][clm]);
+						}
+					}
+				}
+				this.grid = Array(this.gridUHei).fill().map(x => Array(this.gridUWid).fill(null));
+				for (let clm = 0; clm < gridTiles.length; clm++) {
+					let prevPowerOf2 = 0;
+					for (let row = gridTiles[clm].length - 1; row >= 0; row--) {
+						if (gridTiles[clm][row].powerOf2 == prevPowerOf2) {
+							this.deletedTiles.unshift(new Tile(gridTiles[clm][row].uX, gridTiles[clm][row].uY, gridTiles[clm][row].powerOf2));
+							this.deletedTiles[0].siblingTile = gridTiles[clm][row + 1];
+							gridTiles[clm].splice(row, 1);
+							gridTiles[clm][row].changeNumber = true;
+							prevPowerOf2 = 0;
+						} else {
+							prevPowerOf2 = gridTiles[clm][row].powerOf2;
+						}
+					}
+
+					for (let row = 0; row < gridTiles[clm].length; row++) {
+						if (dir == "up") {
+							this.grid[row][clm] = gridTiles[clm][row];
+							updateGridTile(clm, row);
+						} else {
+							this.grid[this.gridUHei - gridTiles[clm].length + row][clm] = gridTiles[clm][row];
+							updateGridTile(clm, this.gridUHei - gridTiles[clm].length + row);
+						}
+					}
+
+				}
+			}
+
+			if (dir == "left" || dir == "right") {
+				gridTiles = Array(this.gridUHei);
+
+				for (let row in this.grid) {
+					gridTiles[row] = [];
+					for (let clm in this.grid[0]) {
+						if (this.grid[row][clm] != null) {
+							gridTiles[row].push(this.grid[row][clm]);
+						}
+					}
+				}
+				this.grid = Array(this.gridUHei).fill().map(x => Array(this.gridUWid).fill(null));
+				for (let row = 0; row < gridTiles.length; row++) {
+					let prevPowerOf2 = 0;
+					for (let clm = gridTiles[row].length - 1; clm >= 0; clm--) {
+						if (gridTiles[row][clm].powerOf2 == prevPowerOf2) {
+							this.deletedTiles.unshift(new Tile(gridTiles[row][clm].uX, gridTiles[row][clm].uY, gridTiles[row][clm].powerOf2));
+							this.deletedTiles[0].siblingTile = gridTiles[row][clm + 1];
+							gridTiles[row].splice(clm, 1);
+							gridTiles[row][clm].changeNumber = true;
+							prevPowerOf2 = 0;
+						} else {
+							prevPowerOf2 = gridTiles[row][clm].powerOf2;
+						}
+					}
+
+					for (let clm = 0; clm < gridTiles[row].length; clm++) {
+						if (dir == "left") {
+							this.grid[row][clm] = gridTiles[row][clm];
+							updateGridTile(clm, row);
+						} else {
+							this.grid[row][this.gridUWid - gridTiles[row].length + clm] = gridTiles[row][clm];
+							updateGridTile(this.gridUWid - gridTiles[row].length + clm, row);
+						}
 					}
 				}
 			}
-			this.grid = Array(this.gridUHei).fill().map(x => Array(this.gridUWid).fill(null));
-			for (let row = 0; row < gridTiles.length; row++) {
-				let prevPowerOf2 = 0;
-				for (let clm = gridTiles[row].length - 1; clm >= 0; clm--) {
-					if (gridTiles[row][clm].powerOf2 == prevPowerOf2) {
-						this.deletedTiles.unshift(new Tile(gridTiles[row][clm].uX, gridTiles[row][clm].uY, gridTiles[row][clm].powerOf2));
-						this.deletedTiles[0].siblingTile = gridTiles[row][clm + 1];
-						gridTiles[row].splice(clm, 1);
-						gridTiles[row][clm].changeNumber = true;
-						prevPowerOf2 = 0;
-					} else {
-						prevPowerOf2 = gridTiles[row][clm].powerOf2;
-					}
-				}
 
-				for (let clm = 0; clm < gridTiles[row].length; clm++) {
-					if (dir == "left") {
-						this.grid[row][clm] = gridTiles[row][clm];
-						updateGridTile(clm, row);
-					} else {
-						this.grid[row][this.gridUWid - gridTiles[row].length + clm] = gridTiles[row][clm];
-						updateGridTile(this.gridUWid - gridTiles[row].length + clm, row);
-					}
-				}
+			for (let i in this.deletedTiles) {
+				this.deletedTiles[i].move(this.deletedTiles[i].siblingTile.uX, this.deletedTiles[i].siblingTile.uY);
 			}
-		}
 
-		for (let i in this.deletedTiles) {
-			this.deletedTiles[i].move(this.deletedTiles[i].siblingTile.uX, this.deletedTiles[i].siblingTile.uY);
+			//console.log(gridTiles, dir);
+			this.tileAdded = false;
 		}
-
-		console.log(gridTiles, dir);
-		this.tileAdded = false;
 	}
 
 }
