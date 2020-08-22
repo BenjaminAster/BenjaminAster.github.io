6d9012cb43b3e5976da6a3e32408c7c887d45781
BenjaminAster <nimajneb0905@gmail.com>
2020-08-22 16:24:14 +0200

    -

--- utils/database.js
+++ utils/database.js
@@ -1,33 +1,58 @@
-includeScript("https://www.gstatic.com/firebasejs/7.19.0/firebase.js", function () {
+var database;
+
+$.getScript("https://www.gstatic.com/firebasejs/7.19.0/firebase.js", function () {
+
+	$.getScript("https://www.gstatic.com/firebasejs/7.19.0/firebase-analytics.js", function () {
 
 
-	console.log("database file included");
 
-	let config = {
-		apiKey: "AIzaSyDS76v4WlBEUbJrQTbDbwFFHdN3n_zqKFg",
-		authDomain: "benjamin-aster.firebaseapp.com",
-		databaseURL: "https://benjamin-aster.firebaseio.com",
-		projectId: "benjamin-aster",
-		storageBucket: "benjamin-aster.appspot.com",
-		messagingSenderId: "1043928494448",
-		appId: "1:1043928494448:web:45725d68d9278747ca1fb5",
-		measurementId: "G-Z96EP3QXLR"
-	};
-	// Initialize Firebase
-	firebase.initializeApp(config);
-	firebase.analytics();
+		console.log("database file included");
 
-	let database = firebase.database();
+		class Database {
+			constructor() {
+				var config = {
+					apiKey: "AIzaSyDS76v4WlBEUbJrQTbDbwFFHdN3n_zqKFg",
+					authDomain: "benjamin-aster.firebaseapp.com",
+					databaseURL: "https://benjamin-aster.firebaseio.com",
+					projectId: "benjamin-aster",
+					storageBucket: "benjamin-aster.appspot.com",
+					messagingSenderId: "1043928494448",
+					appId: "1:1043928494448:web:45725d68d9278747ca1fb5",
+					measurementId: "G-Z96EP3QXLR"
+				};
+				// Initialize Firebase
+				firebase.initializeApp(config);
+				firebase.analytics();
 
-	function writeToDatabase(path, key, value) {
-		database.ref(path).set(
-			eval(`{${key}: ${value}}`)
-		);
-	}
+				this.database = firebase.database();
+			}
 
-	function readFromDatabase(path, key, value) {
+			write(path, object) {
+				//let object = {};
+				//eval(`object.${key} = value`);
+				this.database.ref(path).set(object);
+			}
 
-	}
+			push(path, object) {
+				//let object = {};
+				//eval(`object.${key} = value`);
+				//this.database.ref(path).push().set(object);
+				this.database.ref(path).push(object);
+			}
+
+			read(path, key, callback = function () { }) {
+				let value;
+				this.database.ref(path).once("value", function (snapshot) {
+					value = eval(`snapshot.val().${key}`);
+					callback(value);
+				})
+			}
+		}
+
+		database = new Database();
+
+
+	});
 
 
 });
---
+++ utils/p5.sound.js
@@ -0,0 +1,3 @@
+let imported = document.createElement("script");
+imported.src = "https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.1.9/p5.sound.js";
+document.head.appendChild(imported);
