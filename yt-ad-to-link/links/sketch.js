e7c348977c76be063bb9616669edf9a72b767c8c
BenjaminAster <nimajneb0905@gmail.com>
2020-08-23 15:03:04 +0200

    -

--- yt-ad-to-link/links/sketch.js
+++ yt-ad-to-link/links/sketch.js
@@ -1,14 +1,17 @@
 
 
 function setup() {
-	database.read("/unlistedAdLinks", (value) => {
-		keys = Object.keys(value)
-		document.querySelector("#num-of-links").innerHTML = keys.length;
-		for (let key of keys) {
-			document.querySelector(".urls").innerHTML +=
-				`<li> <span class="url"><a href="https://www.youtube.com/watch?v=${eval(`value["${key}"].id`)}"> youtu.be/${eval(`value["${key}"].id`)}</a></span> ${""
-				}&#9472 <span class="title">${eval(`value["${key}"].title`)}</span></li>`
-		}
-		document.querySelector("#loading-paragraph").hidden = true;
-	});
+	database.on("/unlistedAdLinks", () => {
+		database.read("/unlistedAdLinks", (value) => {
+			keys = Object.keys(value)
+			document.querySelector("#num-of-links").innerHTML = keys.length;
+			document.querySelector("#urls").innerHTML = "";
+			for (let key of keys) {
+				document.querySelector("#urls").innerHTML +=
+					`<li> <span class="url"><a href="https://www.youtube.com/watch?v=${eval(`value["${key}"].id`)}"> youtu.be/${eval(`value["${key}"].id`)}</a></span> ${""
+					}&#9472 <span class="title">${eval(`value["${key}"].title`)}</span></li>`
+			}
+			document.querySelector("#loading-paragraph").hidden = true;
+		});
+	})
 }
