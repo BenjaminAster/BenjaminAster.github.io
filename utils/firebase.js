eb05cd435b281e528b91fca425d3df77dd042008
BenjaminAster <nimajneb0905@gmail.com>
2020-08-31 22:10:30 +0200

    -

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,13 +1,1 @@
-let imported;
-/*
-imported = document.createElement("script");
-imported.src = "https://www.gstatic.com/firebasejs/7.19.1/firebase.js";
-document.head.appendChild(imported);
-*/
-imported = document.createElement("script");
-imported.src = "https://www.gstatic.com/firebasejs/7.19.1/firebase-app.js";
-document.head.appendChild(imported);
-
-imported = document.createElement("script");
-imported.src = "https://www.gstatic.com/firebasejs/7.19.1/firebase-analytics.js";
-document.head.appendChild(imported);
+$("head").apppend(document.createElement("script"));
