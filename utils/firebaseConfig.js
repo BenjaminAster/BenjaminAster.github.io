8287de7f277c03786d7f63a2499de514143568a2
BenjaminAster <nimajneb0905@gmail.com>
2020-08-31 13:30:14 +0200

    -

---
+++ utils/firebaseConfig.js
@@ -0,0 +1,15 @@
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: "AIzaSyDS76v4WlBEUbJrQTbDbwFFHdN3n_zqKFg",
+	authDomain: "benjamin-aster.firebaseapp.com",
+	databaseURL: "https://benjamin-aster.firebaseio.com",
+	projectId: "benjamin-aster",
+	storageBucket: "benjamin-aster.appspot.com",
+	messagingSenderId: "1043928494448",
+	appId: "1:1043928494448:web:45725d68d9278747ca1fb5",
+	measurementId: "G-Z96EP3QXLR"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
